b90e65914ba57c41ebd00261b5a68205f628f48b
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-07 19:56:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |attribute, value|
+  if attribute == :fourth_of_july
+    value.each do |BBQ|
+      puts "#{BBQ}"
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
